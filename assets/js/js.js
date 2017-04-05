e83719965ff3cad3f7790d69f26be077a873318e
ulamos <yulia.moss@gmail.com>
2017-04-05 20:24:47 +0000

    second commit

---
+++ assets/js/js.js
@@ -0,0 +1,17 @@
+// JavaScript File
+$(document).ready(function() {
+    $('.bxslider').bxSlider({
+        auto: true,
+        mode: 'fade',
+        captions: true,
+        speed: 1000,
+        pause:2000,
+        autoControls: true
+        // autoHover: true
+    });
+});
+
+
+
+// Pluso_icons
+
